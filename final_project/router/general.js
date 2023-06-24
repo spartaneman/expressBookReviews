1b2617abacd3e8f4144fc6575d52d88c0a18f813
spartaneman <spartaneman@gmail.com>
2023-06-24 14:54:47 -0700

    Must convert book from object to array

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,79 +1,98 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+books = Object.values(books);
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   const username = req.params.username;
   const password = req.params.username;
 
-  if(!username || !password){
-    return res.status(404).json({status: "Failed", message: "Missing Information"});
+  if (!username || !password) {
+    return res
+      .status(404)
+      .json({ status: "Failed", message: "Missing Information" });
   }
-  if(isValid(username)){
-    users.push({"username":username, "password":password});
-    return res.status(200).json({status: "success", message: "User Registered"});
-  }
-  else{
-    return res.status(208).json({status: "fail", message: "User already exists"});
+  if (isValid(username)) {
+    users.push({ username: username, password: password });
+    return res
+      .status(200)
+      .json({ status: "success", message: "User Registered" });
+  } else {
+    return res
+      .status(208)
+      .json({ status: "fail", message: "User already exists" });
   }
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(JSON.stringify({
-    status: "success", data: {books}}));
+public_users.get("/", function (req, res) {
+  return res.status(200).json(
+    JSON.stringify(
+      {
+        status: "success",
+        data: { books },
+      },
+      null,
+      1
+    )
+  );
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  let iBooks = books.filter((book)=>{
-    return book.isbn === isbn
-  });
-  if(iBooks.length > 0){
-    return res.status(200).json(JSON.stringify({status: "success", data: {iBooks}}))
+  let iBooks = books[isbn];
+  if (iBooks) {
+    return res
+      .status(200)
+      .json(JSON.stringify({ status: "success", data: { iBooks } }));
+  } else {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "ISBN not found in our database" });
   }
-  else{
-    return res.status(404).json({status: "fail", message: "ISBN not found in our database"});
-  }
-  
- });
-  
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get("/author/:author", function (req, res) {
   const author = req.params.author;
-  let iBooks = books.filter((book)=>{
-    return book.author === author
+  let iBooks = books.filter((book) => {
+    return book.author === author;
   });
-  if(iBooks.length > 0){
-    return res.status(200).json(JSON.stringify({status: "success", data: {iBooks}}))
-  }
-  else{
-    return res.status(404).json({status: "fail", message: "Author not found in our database"});
+  if (iBooks.length > 0) {
+    return res
+      .status(200)
+      .json(JSON.stringify({ status: "success", data: { iBooks } }));
+  } else {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "Author not found in our database" });
   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", function (req, res) {
   const title = req.params.title;
-  let iBooks = books.filter((book)=>{
-    return book.title === title
+  let iBooks = books.filter((book) => {
+    return book.title === title;
   });
-  if(iBooks.length > 0){
-    return res.status(200).json(JSON.stringify({status: "success", data: {iBooks}}))
-  }
-  else{
-    return res.status(404).json({status: "fail", message: "Title not found in our database"});
+  if (iBooks.length > 0) {
+    return res
+      .status(200)
+      .json(JSON.stringify({ status: "success", data: { iBooks } }));
+  } else {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "T not found in our database" });
   }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
