dee0fe030e3192219d9bf1f84497cd38b39295ec
spartaneman <spartaneman@gmail.com>
2023-06-24 14:27:02 -0700

    completed get books, by isbn, author, title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,26 +23,51 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(JSON.stringify({
+    status: "success", data: {books}}));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let iBooks = books.filter((book)=>{
+    return book.isbn === isbn
+  });
+  if(iBooks.length > 0){
+    return res.status(200).json(JSON.stringify({status: "success", data: {iBooks}}))
+  }
+  else{
+    return res.status(404).json({status: "fail", message: "ISBN not found in our database"});
+  }
+  
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let iBooks = books.filter((book)=>{
+    return book.author === author
+  });
+  if(iBooks.length > 0){
+    return res.status(200).json(JSON.stringify({status: "success", data: {iBooks}}))
+  }
+  else{
+    return res.status(404).json({status: "fail", message: "Author not found in our database"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let iBooks = books.filter((book)=>{
+    return book.title === title
+  });
+  if(iBooks.length > 0){
+    return res.status(200).json(JSON.stringify({status: "success", data: {iBooks}}))
+  }
+  else{
+    return res.status(404).json({status: "fail", message: "Title not found in our database"});
+  }
 });
 
 //  Get book review
